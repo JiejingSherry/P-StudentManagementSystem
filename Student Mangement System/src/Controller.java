afcb73272e02a500f3d552b72ffaa98707f56515
Jiejing <Jiejing@GYC_ProBook>
2016-08-02 12:40:47 +1200

    Add Controller Class

--- Student Mangement System/src/Controller.java
+++ Student Mangement System/src/Controller.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+
 public class Controller {
 	private static Controller controller = null;
 	private List<Student> stus = new ArrayList<Student>();
